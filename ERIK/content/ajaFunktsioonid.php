b7da38a3f43495f51c18251c0591b6a076efd418
tugosera <erik.gerega@gmail.com>
2024-11-08 13:24:48 +0200

    .

--- ERIK/content/ajaFunktsioonid.php
+++ ERIK/content/ajaFunktsioonid.php
@@ -1,1 +1,110 @@
 <?php
+echo "<h2>Ajafunktsioonid</h2>";
+echo "<div id='kuupaev'>";
+echo "Täna on ".date("d.m.Y.")."<br>";
+date_default_timezone_set("Europe/Tallinn");
+echo "<strong>";
+echo "Tanane Tallinna kuupäev ja kellaeag on".
+    date("d.m.Y G:i", time())."<br>";
+echo "</strong>";
+echo "date('d.m.Y G:i', time())";
+echo "<br>";
+echo "d - kuupäev 1-31";
+echo "<br>";
+echo "m - kuu numbrina 1- 12";
+echo "<br>";
+echo "Y - aasta neljakohane";
+echo "<br>";
+echo "G - tunniformaat 0-24";
+echo "<br>";
+echo "i - minutid 0-59";
+echo "</div>";
+?>
+<div id="hooaeg">
+    <h2>Väljasta vastavalt hooajale pilt (kevad/suvi/sügis/talv)</h2>
+    <?php
+    $today = new DateTime();
+    echo "Täna on ".$today->format('3-30-2024');
+    //hooaja punktid - сезон
+    $spring=new DateTime('March 20');
+    $summer=new DateTime('june 21');
+    $fall=new DateTime('september 22');
+    $winter=new DateTime('December 22');
+
+    switch(true){
+        //kevad
+        case $today>=$spring && $today<$summer:
+            echo ", Kevad ";
+            echo "<br>";
+            $pildi_aadress='content/img/kevad.jpg';
+            break;
+        case $today>=$summer && $today<$fall:
+            echo ", Suvi ";
+            echo "<br>";
+            $pildi_aadress='content/img/suvi.jpg';
+            break;
+        case $today>=$fall && $today<$winter:
+            echo ", Sügis ";
+            echo "<br>";
+            $pildi_aadress='content/img/sügis.jpg';
+            break;
+        default:
+            echo ", Talv ";
+            echo "<br>";
+            $pildi_aadress='content/img/talv.jpg';
+            break;
+    }
+    ?>
+    <img src="<?=$pildi_aadress?>" alt='hooaja pilt' width = "300px">
+</div>
+<div id="koolivabaaeg">
+    <h2>Mittu päeva on koolivabaajani 23.12.2024</h2>
+    <?php
+    $kdate=date_create_from_format('d.m.Y', '23.12.2024');
+    $date=date_create();
+    $diff=date_diff($kdate,$date);
+    echo "jääb ".$diff->format("%a")." päeva" ;
+    echo "<br>";
+    echo "jääb ".$diff->days." päeva" ;
+    ?>
+</div>
+<div id="sunnipaev">
+    <h2>Mittu päeva on minu sünnipäevani 17.09.2025</h2>
+    <?php
+    $sdate=date_create_from_format('d.m.Y', '17.09.2025');
+    $date=date_create();
+    $diff=date_diff($sdate,$date);
+    echo "jääb ".$diff->format("%a")." päeva" ;
+    echo "<br>";
+    echo "jääb ".$diff->days." päeva" ;
+    ?>
+</div>
+<div id="vanus">
+    <h2>Kasutaja vanuse leidmine</h2>
+    <form method="post">
+        Sisesta ima sünnikuupäev
+        <input type="date" name="synd" placeholder="dd.mm.yyyy">
+        <input type="submit" value="OK">
+    </form>
+    <?php
+    if(empty($_POST['synd'])){
+        echo "sisesta oma sünnipäeva kuupäev";
+    }
+    else{
+        $ssdate=date_create($_POST['synd']);
+        $date=date_create();
+        $diff=date_diff($ssdate,$date);
+        echo "sa oled ".$diff->format("%y")." aastat vana" ;
+    }
+    ?>
+</div>
+<div>
+    <h2>Massivi abil näidata kuu nimega tänases kuupäevas</h2>
+    <?php
+    $kuud=array(1=>'jaanuar','veebruar','märts','aprill','mai'.'juuni','juuli','august','september','oktoober','november','december');
+    $paev=date('d');
+    $year=date('y');
+    $kuu=$kuud[date('n')];
+    echo $paev.".".$kuu.".".$year;
+    ?>
+</div>
--- ERIK/content/moistatus.php
+++ ERIK/content/moistatus.php
@@ -1,15 +1,39 @@
 <?php
 echo "Mõistatus: Euroopa riik<br>";
-$riik = "Saksamaa";
-$pikkus = strlen($riik);
-$kasutatudI = [];
+$riik = "saksamaa";
+$otsing = "on";
 
-while (count($kasutatudI) < 4) {
-    $rand = random_int(0, $pikkus - 1);
-    if (!in_array($rand, $kasutatudI)) {
-        $kasutatudI[] = $rand;
-        echo ($rand + 1)." täht on ".$riik[$rand];
-        echo "<br>";
+echo "Esimene täht - ".substr($riik, 0, 1);
+
+echo "<br>";
+
+echo "Riigi nimi pikkus - ".strlen($riik);
+echo '<br>';
+
+?>
+
+<form method="post">
+    Sisesta vastus
+    <input type="text" name="riig">
+    <input type="submit" value="OK">
+</form>
+
+<?php
+if(empty($_POST['riig']))
+{
+    echo " ";
+}
+else{
+    if($_POST['riig']==$riik)
+    {
+        echo "Sul on õigus";
+    }
+    else
+    {
+        echo "ei ole õige";
     }
 }
+
+echo "<br>";
+//highlight_file('moistatus.php');
 ?>
